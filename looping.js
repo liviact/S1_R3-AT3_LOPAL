e510d4da9d7b5cd2fd73f5ec09fc58434bd2a6c7
unknown <lilicaetano19@gmail.com>
2025-04-22 13:29:28 -0300

    feito o ex3

--- looping.js
+++ looping.js
@@ -1,13 +1,15 @@
-let soma = 0;
-
-// for para pedir 10 números
-for (let i = 1; i <= 10; i++) {
-    // Solicita ao usuário para digitar um número
-    let numero = parseInt(prompt(`Digite o ${i}º número:`));
-    
-    // Adiciona o número à soma
-    soma += numero;
+// Solicita ao usuário um número
+let numero = parseInt(prompt("Digite um número para ver sua tabuada:"));
+if (isNaN(numero)){
+    alert ("digite um número válido");
 }
 
-// Exibe a soma total dos números
-alert(`A soma dos 10 números é: ${soma}`);
+let resultado;
+let frase="";
+// Exibe a tabuada do número de 1 a 10
+for (let i = 1; i <= 10; i++) {
+    resultado= numero * i;
+    frase +=`${numero} * ${i} = ${resultado} \n`;
+}
+
+alert (frase);
