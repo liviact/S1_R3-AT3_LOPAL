1ac436d23e68528f3781e55ac4bdea107d3ca68e
unknown <lilicaetano19@gmail.com>
2025-04-22 12:48:54 -0300

    corrigido o ex1

--- looping.js
+++ looping.js
@@ -1,15 +1,13 @@
-// constante que armazenará o número solicitado
+const num = parseInt(prompt("Digite um número para eu dizer os números até ele:"));
 
-const numero = parseInt(prompt("digite um número, por favor:"));
-
-if (isNaN(numero) || numero<1 ) {
-    alert("você não escreveu um número válido.");
+if (isNaN(num) || num <= 1) {
+    alert("ERRO: você não digitou um número válido!");
 } else {
-    let numUm = 0
-    for (let i = 1 ; i <= numero; i++) {
-        numUm+=`${i}\n`
+    let numeros = ""; // variável para guardar os números
+
+    for (let i = 1; i <= num; i++) {
+        numeros += i + "\n"; // adiciona número com quebra de linha
     }
-    alert(`os numeros até ${numero} são ${numUm}`);
 
-
+    console.log(`Números de 1 até ${num} são:\n${numeros}`);
 }
